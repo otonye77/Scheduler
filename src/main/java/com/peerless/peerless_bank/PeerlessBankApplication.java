a019f44d8c130a891784acbf830392a8b7563687
Otonye77 <oamietubodie@gmail.com>
2025-01-30 23:30:57 +0100

    first commit

---
+++ src/main/java/com/peerless/peerless_bank/PeerlessBankApplication.java
@@ -0,0 +1,13 @@
+package com.peerless.peerless_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerlessBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeerlessBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peerless/peerless_bank/PeerlessBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peerless.peerless_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeerlessBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
